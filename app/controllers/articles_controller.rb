f83ee35ff7d54582032d92ebf76cbbaca4025de1
Sumanthmadishetty <sumanth5812@GMAIL.COM>
2018-05-02 21:38:40 +0530

    Initial Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,73 @@
+class ArticlesController < ApplicationController
+    
+    before_action :set_article,only: [:show,:edit,:update,:destroy]
+    
+    
+    
+    def new
+
+        @article=Article.new
+    end
+    def create
+        @article = Article.new(article_params)
+ 
+         if   @article.save
+                
+            flash[:notice]="Article created succesfully"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+
+    
+        
+    end
+    def index
+       @article=Article.all
+    end
+
+
+    def show
+        
+    end
+    def edit
+
+    
+    end
+        
+    def update
+        
+        
+        
+        if @article.update(article_params)
+        
+            flash[:notice] = "Article was successfully updated"
+        
+            redirect_to article_path(@article)
+        
+        else
+        
+             render 'edit'
+        
+        end
+        
+    end
+    def destroy
+       
+        @article.destroy
+        flash[:notice] = "Article was deleted sucessfully"
+        redirect_to articles_path
+    end
+
+    
+    private 
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
+    def set_article
+        @article=Article.find(params[:id])
+    end
+
+    
+
+end
