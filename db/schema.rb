835e7293f455975095e544abe4510fbe1933259e
Sumanthmadishetty <sumanth5812@GMAIL.COM>
2018-05-08 23:08:48 +0530

    user id articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_155323) do
+ActiveRecord::Schema.define(version: 2018_05_08_164445) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
